8636843c68438d9f58cfd3aabf5e01ac3128e0ac
Hakan Kurt <hakan.kurt@portalgrup.com>
2025-01-13 16:15:10 +0300

    Update main and room1 & add room utilities

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -1,1 +1,9 @@
-export function room1() {}
+import { setBackgroundColor } from "./roomUtils.js";
+
+export function room1(k, roomData) {
+  setBackgroundColor(k, "#a2aed5");
+
+  const roomLayers = roomData.layers;
+
+  const map = k.add([k.pos(0, 0), k.sprite("room1")]);
+}
