67471a08b2442374e6e9576fdb239d8e27da3423
Hakan Kurt <hakan.kurt@portalgrup.com>
2025-01-15 14:49:21 +0300

    Update make player & Add global state manager

---
+++ src/entities/player.js
@@ -0,0 +1,21 @@
+import { state } from "../state/globalStateManager.js";
+
+export function makePlayer(k, initialPos) {
+  return k.make([
+    k.pos(initialPos),
+    k.sprite("player"),
+    k.area({
+      shape: new k.Rect(k.vec2(0, 18), 12, 12),
+    }),
+    k.anchor("center"),
+    k.body({ mass: 100, jumpForce: 320 }),
+    k.doubleJump(state.current().isDoubleJumpUnlocked ? 2 : 1),
+    k.opacity(),
+    k.health(state.current().playerHp),
+    "player",
+    {
+      speed: 150,
+      isAttacking: false,
+    },
+  ]);
+}
