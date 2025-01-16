00f27ba082a96727b8ef37f7babb416e3d71a29a
Hakan Kurt <hakan.kurt@portalgrup.com>
2025-01-16 12:40:25 +0300

    Add control handlers for make player

--- src/entities/player.js
+++ src/entities/player.js
@@ -16,6 +16,46 @@ export function makePlayer(k, initialPos) {
     {
       speed: 150,
       isAttacking: false,
+      setControls() {
+        this.controlHandlers = [];
+
+        this.controlHandlers.push(
+          k.onKeyPress((key) => {
+            if (key === "x") {
+              if (this.curAnim() !== "jump") this.play("jump");
+              this.doubleJump();
+            }
+
+            if (
+              key === "z" &&
+              this.curAnim() !== "attack" &&
+              this.isGrounded()
+            ) {
+              this.isAttacking = true;
+              this.add([
+                k.pos(this.flipX ? -25 : 0, 10),
+                k.area({
+                  shape: new k.Rect(k.vec2(0), 25, 10),
+                }),
+                "sword-hitbox",
+              ]);
+              this.play("attack");
+
+              this.onAnimEnd((anim) => {
+                if (anim === "attack") {
+                  const swordHitbox = k.get("sword-hitbox", {
+                    recursive: true,
+                  })[0];
+
+                  if (swordHitbox) k.destroy(swordHitbox);
+                  this.isAttacking = false;
+                  this.play("idle");
+                }
+              });
+            }
+          })
+        );
+      },
     },
   ]);
 }
